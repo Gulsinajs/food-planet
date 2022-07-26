2c09d18df3aec16a848abd47eeecae50557243ae
gulsina <gulsina.zh>
2022-07-26 23:31:37 +0600

    feat: add NewProducts

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Main from "./pages/Main/Main";
 import Content from "./components/Content/Content";
+import NewProducts from "./components/NewProducts/NewProducts";
 
 
 function App() {
     return (
         <>
             <Header/>
+            <Content/>
+            <NewProducts/>
             <BrowserRouter>
                 <Routes>
                     <Route path="" element={<Main/>}/>
                 </Routes>
             </BrowserRouter>
-            <Content/>
         </>
     );
 }
