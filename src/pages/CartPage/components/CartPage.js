b5ab64abaf85671d11ba9df1b452de9d57181e74
gulsina <gulsina.zh>
2022-08-09 18:05:25 +0600

    feat: added styles CartPage

--- src/pages/CartPage/components/CartPage.js
+++ src/pages/CartPage/components/CartPage.js
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom";
 import styles from './CartPage.module.css';
 import xCircle from '../../../media/icons/x-circle.png';
 import Back from '../../../media/icons/back-arroy.png';
+import burger from '../../../media/images/firstBurger.png';
 
 const CartPage = () => {
     return (
@@ -12,37 +13,28 @@ const CartPage = () => {
                 <h4>Товары в вашей корзине</h4>
             </div>
             <table>
-                <tr>
-                    <td colSpan={2}>Продукт</td>
-                    <td>Количество</td>
-                    <td>Цена</td>
-                    <td>К оплате</td>
+                <tr className={styles.products_title}>
+                    <th colSpan={2}>Продукт</th>
+                    <th>Количество</th>
+                    <th>Цена</th>
+                    <th>К оплате</th>
                 </tr>
-                <tr>
-                    <td><img src="" alt=""/></td>
+                <tr className={styles.products_in_cart}>
+                    <td><img src={burger} alt=""/></td>
                     <td><p>burger</p></td>
                     <td><input type="number"/></td>
                     <td><p><span></span> сом</p></td>
                     <td><p><span></span> сом</p></td>
-                    <td><a href=""><img src={xCircle} alt=""/></a></td>
+                    <td className={styles.delete_in_cart}><a href=""><img src={xCircle} alt=""/></a></td>
                 </tr>
-                <tr>
-                    <td colSpan={4}><p>Итого: </p></td>
-                    <td><p><span></span> сом</p></td>
+                <tr className={styles.count_element}>
+                    <th colSpan={4}><p>Итого: </p></th>
+                    <th><p><span></span> сом</p></th>
                 </tr>
             </table>
-            <div className={styles.products_container}>
-                <div className={styles.products_title}>
-                </div>
-                <div className={styles.products_in_cart}>
-                </div>
-
-                <div className={styles.count_element}>
-                </div>
-                <div className={styles.place_order}>
-                    <Link to="/menu"><img src={Back} alt=""/>Продолжить покупки</Link>
-                    <button>Оформить заказ</button>
-                </div>
+            <div className={styles.place_order}>
+                <Link to="/"><img src={Back} alt=""/>Продолжить покупки</Link>
+                <button>Оформить заказ</button>
             </div>
         </div>
     );
